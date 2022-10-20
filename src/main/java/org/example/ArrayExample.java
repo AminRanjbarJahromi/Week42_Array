4aeaa493efa897019f8649fbb81ae6e3b277d074
Amin Ranjbar <m.amin.ranjbar@gmail.com>
2022-10-20 10:29:37 +0200

    swapping array elements (sort)1

---
+++ src/main/java/org/example/ArrayExample.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class ArrayExample {
+
+    private static String[] names = {"Amin", "Hamid","Mehrdad", "Mohsen"};
+
+    public static String findName( String nameToFind){
+
+        for(String elements : names){
+            if(elements.equalsIgnoreCase(nameToFind)){
+                return elements;
+            }
+        }
+        return "No match";
+    }
+
+
+
+}
